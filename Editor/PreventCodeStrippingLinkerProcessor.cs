14f4fdcd9174a51480f7e64dc535cfe8729e6d66
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2025-12-23 11:27:17 +0100

    auto-find preservable types

--- Editor/PreventCodeStrippingLinkerProcessor.cs
+++ Editor/PreventCodeStrippingLinkerProcessor.cs
@@ -1,54 +1,29 @@
-﻿using Luny.Reflection;
-using LunyEditor;
+﻿using LunyEditor;
 using LunyScript;
 using System;
-using System.Collections.Generic;
-
-namespace LunyScript.Unity {} // placeholder
 
 namespace LunyScriptEditor
 {
 	internal sealed class PreventCodeStrippingLinkerProcessor : LunyLinkerProcessor
 	{
-		private static List<PreserveDetails> PreserveAllLunyScriptTypes()
-		{
-			var details = new List<PreserveDetails>();
-
-			var preserveScripts = new Dictionary<String, List<String>>();
-			foreach (var scriptType in TypeDiscovery.FindAll<LunyScript.LunyScript>())
-			{
-				var assemblyName = scriptType.Assembly.GetName().Name;
-				if (preserveScripts.ContainsKey(assemblyName) == false)
-					preserveScripts[assemblyName] = new List<String>();
-
-				preserveScripts[assemblyName].Add(scriptType.FullName);
-			}
-
-			foreach (var pair in preserveScripts)
-				details.Add(new PreserveDetails { Assembly = pair.Key, Types = pair.Value.ToArray() });
-
-			return details;
-		}
-
 		public override PreserveDetails[] GetPreserveDetails()
 		{
-			var details = PreserveAllLunyScriptTypes();
+			// preserve all user scripts across assemblies (any scripts in Editor assemblies will not be in build)
+			var details = PreserveAllDerivedClasses<LunyScript.LunyScript>();
 
 			details.Add(new PreserveDetails
 			{
 				Assembly = nameof(LunyScript),
-				Types = new []
+				Types = new[]
 				{
 					// script runner is discovered through reflection
 					typeof(LunyScriptRunner).FullName,
-				}
+				},
 			});
-
-			//details.Add(new PreserveDetails { Assembly = $"{nameof(LunyScript)}.{nameof(LunyScript.Unity)}" });
 
 			return details.ToArray();
 		}
 
-		public override String GetLinkFilename() => nameof(LunyScript);
+		public override String GetAssemblyName() => $"{nameof(LunyScript)}.Unity";
 	}
 }
