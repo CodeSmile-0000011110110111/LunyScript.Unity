d151e65b432f18a78231617f2e82c0cf69811cf0
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2026-01-23 19:28:08 +0100

    move object test script to agnostic LunyScript

--- SmokeTests/Scripts/ObjectTestController.cs
+++ SmokeTests/Scripts/ObjectTestController.cs
@@ -49,22 +49,4 @@ namespace LunyScript.Unity.SmokeTests
 			AssertObjectsCreated();
 		}
 	}
-
-	public sealed class ObjectTestLunyScript : LunyScript
-	{
-		public const String DestroyedObjectName = "destroyed";
-		public const String EmptyObjectName = "empty";
-		public const String CubeObjectName = "cube";
-		public const String SphereObjectName = "sphere";
-
-		public override void Build()
-		{
-			When.Self.Created(Object.CreateEmpty(DestroyedObjectName));
-			When.Self.Updates(Object.Destroy(DestroyedObjectName));
-
-			When.Self.Ready(Object.CreateEmpty(EmptyObjectName));
-			When.Self.Ready(Object.CreateCube(CubeObjectName));
-			When.Self.Ready(Object.CreateSphere(SphereObjectName));
-		}
-	}
 }
