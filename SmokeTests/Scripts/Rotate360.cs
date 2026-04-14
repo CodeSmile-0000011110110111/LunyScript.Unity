622b5385ce00f13bfb010c057ef54abe3b71f45b
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2026-04-14 13:35:01 +0200

    MoveTowards demo

---
+++ SmokeTests/Scripts/Rotate360.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+
+namespace LunyScript
+{
+	public sealed class Rotate360 : MonoBehaviour
+	{
+		[SerializeField] private Vector3 _angles;
+		[SerializeField] private Single _timeScale = 1f / 60f;
+
+		private Quaternion _rotation;
+
+		private void Awake()
+		{
+			if (Mathf.Approximately(_timeScale, 0f))
+				_timeScale = 1f;
+
+			UpdateRotation();
+		}
+
+		private void OnValidate() => UpdateRotation();
+		private void Update() => transform.localRotation *= _rotation;
+		private void UpdateRotation()
+		{
+			_rotation = Quaternion.Euler(_angles * _timeScale);
+		}
+	}
+}
