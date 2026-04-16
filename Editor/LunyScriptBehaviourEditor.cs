6b32712c70f03b28e52ac7053fc4efda90bfe072
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2026-04-16 17:28:50 +0200

    checkin before adding inspector editable variables

---
+++ Editor/LunyScriptBehaviourEditor.cs
@@ -0,0 +1,38 @@
+﻿using LunyScript.Unity;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+namespace LunyScript.UnityEditor
+{
+	[CustomEditor(typeof(LunyScriptBehaviour))]
+	public class LunyScriptBehaviourEditor : Editor
+	{
+		public override VisualElement CreateInspectorGUI()
+		{
+			var root = new VisualElement();
+
+			// Flatten the nested Data elements
+			var dataProp = serializedObject.FindProperty("_data");
+			if (dataProp != null)
+			{
+				var child = dataProp.Copy();
+				var endProperty = dataProp.GetEndProperty();
+
+				if (child.NextVisible(true) && !SerializedProperty.EqualContents(child, endProperty))
+				{
+					do
+					{
+						var field = new PropertyField(child.Copy());
+						field.Bind(serializedObject);
+						root.Add(field);
+					} while (child.NextVisible(false) && !SerializedProperty.EqualContents(child, endProperty));
+				}
+			}
+			else
+				root.Add(new Label("Could not find _data property."));
+
+			return root;
+		}
+	}
+}
---
+++ LunyScriptBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using Luny;
+using System;
+using UnityEngine;
+
+namespace LunyScript.Unity
+{
+	public sealed class LunyScriptBehaviour : MonoBehaviour
+	{
+		[SerializeField] private ScriptInspectorData _data = new();
+
+		private void Awake()
+		{
+			var entityId = (Int32)GetEntityId();
+			var scriptContext = ScriptEngine.Instance.GetScriptContext(entityId);
+
+		}
+	}
+}
