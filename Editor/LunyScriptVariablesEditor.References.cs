236045f0685619a411d7cc109d720fe60aa3a991
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2026-04-22 17:57:49 +0200

    fix a few variables inspector reference issues

--- Editor/LunyScriptVariablesEditor.References.cs
+++ Editor/LunyScriptVariablesEditor.References.cs
@@ -1,7 +1,6 @@
 ﻿using Luny;
 using LunyScript.Unity;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
 using UnityEditor.UIElements;
@@ -13,16 +12,17 @@ namespace LunyScript.UnityEditor
 {
 	internal sealed partial class LunyScriptVariablesEditor
 	{
-		private readonly List<InspectorReference> _refViewRows = new();
-
 		private MultiColumnListView _referencesListView;
 		private Button _btnRefAdd;
 		private Button _btnRefRemove;
+
+		private Int32 _refCount;
 
 		private static Type GetObjectTypeForRefType(EngineReferenceType refType) => refType switch
 		{
 			EngineReferenceType.GameObject => typeof(GameObject),
 			EngineReferenceType.ScriptableObject => typeof(ScriptableObject),
+			EngineReferenceType.Component => typeof(Component),
 
 			EngineReferenceType.Transform => typeof(Transform),
 			EngineReferenceType.Rigidbody => typeof(Rigidbody),
@@ -31,6 +31,10 @@ namespace LunyScript.UnityEditor
 			EngineReferenceType.AudioClip => typeof(AudioClip),
 			var _ => typeof(Object),
 		};
+
+		private SerializedProperty GetRefsProp() => serializedObject.FindProperty("_references");
+
+		private SerializedProperty GetRefElemProp(Int32 index) => GetRefsProp().GetArrayElementAtIndex(index);
 
 		private void CreateReferencesGUI(VisualElement root)
 		{
@@ -48,19 +52,16 @@ namespace LunyScript.UnityEditor
 		private void OnReferencesSerializedObjectChanged(SerializedObject so)
 		{
 			var refsProp = so.FindProperty("_references");
-			if (refsProp != null && refsProp.arraySize != _refViewRows.Count)
+			if (refsProp != null && refsProp.arraySize != _refCount)
 				RefreshReferences();
 		}
 
 		private void RefreshReferences()
 		{
-			_refViewRows.Clear();
+			serializedObject.Update();
+			_refCount = GetRefsProp().arraySize;
 
-			var component = (LunyScriptVariables)target;
-			foreach (var r in component.References)
-				_refViewRows.Add(r);
-
-			_referencesListView.itemsSource = _refViewRows;
+			_referencesListView.itemsSource = Enumerable.Range(0, _refCount).ToList();
 			_referencesListView.RefreshItems();
 		}
 
@@ -69,125 +70,124 @@ namespace LunyScript.UnityEditor
 			var nameCol = _referencesListView.columns["ref-name"];
 			nameCol.makeCell = () => new TextField { style = { flexGrow = 1 } };
 			nameCol.bindCell = BindRefNameCell;
-			nameCol.unbindCell = (element, _) => ((TextField)element).UnregisterValueChangedCallback(OnRefNameFieldChanged);
+			nameCol.unbindCell = (element, _) => ((TextField)element).Unbind();
 
 			var valueCol = _referencesListView.columns["ref-value"];
 			valueCol.makeCell = () => new VisualElement { style = { flexGrow = 1 } };
 			valueCol.bindCell = BindRefValueCell;
-			valueCol.unbindCell = (element, _) => element.Clear();
+			valueCol.unbindCell = (element, _) =>
+			{
+				element.Q<BindableElement>()?.Unbind();
+				element.Clear();
+			};
 
 			var typeCol = _referencesListView.columns["ref-type"];
 			typeCol.makeCell = () => new VisualElement { style = { flexGrow = 1 } };
 			typeCol.bindCell = BindRefTypeCell;
-			typeCol.unbindCell = (element, _) => element.Clear();
+			typeCol.unbindCell = (element, _) =>
+			{
+				element.Q<BindableElement>()?.Unbind();
+				element.Clear();
+			};
 		}
 
 		private void BindRefNameCell(VisualElement element, Int32 index)
 		{
 			var tf = (TextField)element;
-			tf.SetValueWithoutNotify(_refViewRows[index].Name ?? String.Empty);
 			tf.isReadOnly = Application.isPlaying;
-			tf.userData = index;
 
-			if (!Application.isPlaying)
-				tf.RegisterValueChangedCallback(OnRefNameFieldChanged);
+			var nameProp = GetRefElemProp(index).FindPropertyRelative(nameof(InspectorReference.Name));
+			if (Application.isPlaying)
+				tf.SetValueWithoutNotify(nameProp.stringValue ?? String.Empty);
+			else
+				tf.BindProperty(nameProp);
 
 			EnsureCellInputSelectsRow(_referencesListView, element, index);
-		}
-
-		private void OnRefNameFieldChanged(ChangeEvent<String> evt)
-		{
-			if (Application.isPlaying)
-				return;
-
-			var tf = (TextField)evt.target;
-			if (tf?.userData is not Int32 index || index < 0 || index >= _refViewRows.Count)
-				return;
-
-			var so = serializedObject;
-			so.Update();
-			var refsProp = so.FindProperty("_references");
-			var elem = refsProp.GetArrayElementAtIndex(index);
-			elem.FindPropertyRelative(nameof(InspectorReference.Name)).stringValue = evt.newValue;
-			so.ApplyModifiedProperties();
 		}
 
 		private void BindRefValueCell(VisualElement container, Int32 index)
 		{
 			container.Clear();
 
-			var row = _refViewRows[index];
+			var elem = GetRefElemProp(index);
+			var refTypeProp = elem.FindPropertyRelative(nameof(InspectorReference.RefType));
+			var refType = (EngineReferenceType)refTypeProp.enumValueIndex;
+
 			VisualElement valueElement;
 
-			switch (row.RefType)
+			switch (refType)
 			{
 				case EngineReferenceType.Color:
 					var cf = new ColorField { style = { flexGrow = 1 } };
-					cf.SetValueWithoutNotify(row.ColorValue);
-					cf.SetEnabled(!Application.isPlaying);
-					cf.RegisterValueChangedCallback(evt => SetRefColorValue(index, evt.newValue));
+					cf.BindProperty(elem.FindPropertyRelative(nameof(InspectorReference.ColorValue)));
 					valueElement = cf;
 					break;
 				case EngineReferenceType.AnimationCurve:
 					var curvef = new CurveField { style = { flexGrow = 1 } };
-					curvef.SetValueWithoutNotify(row.CurveValue);
-					curvef.SetEnabled(!Application.isPlaying);
-					curvef.RegisterValueChangedCallback(evt => SetRefCurveValue(index, evt.newValue));
+					curvef.BindProperty(elem.FindPropertyRelative(nameof(InspectorReference.CurveValue)));
 					valueElement = curvef;
 					break;
 				case EngineReferenceType.Vector3:
 					var v3f = new Vector3Field { style = { flexGrow = 1 } };
-					v3f.SetValueWithoutNotify(row.Vector3Value);
-					v3f.SetEnabled(!Application.isPlaying);
-					v3f.RegisterValueChangedCallback(evt => SetRefVector3Value(index, evt.newValue));
+					v3f.BindProperty(elem.FindPropertyRelative(nameof(InspectorReference.Vector3Value)));
 					valueElement = v3f;
 					break;
-				case EngineReferenceType.Rigidbody:
-				case EngineReferenceType.Transform:
-				case EngineReferenceType.Material:
-				case EngineReferenceType.AudioClip:
-				case EngineReferenceType.Component:
-				case EngineReferenceType.GameObject:
-				case EngineReferenceType.ScriptableObject:
-				case EngineReferenceType.Object:
-					var objField = new ObjectField { style = { flexGrow = 1 } };
-					objField.objectType = GetObjectTypeForRefType(row.RefType);
-					objField.allowSceneObjects = true;
-					objField.SetValueWithoutNotify(row.RefValue);
-					objField.SetEnabled(!Application.isPlaying);
-					objField.RegisterValueChangedCallback(evt => SetRefObjectValue(index, evt.newValue));
-					valueElement = objField;
-					break;
 				default:
-					valueElement = new Label($"unhandled type: {row.RefType}");
+					var objField = new ObjectField { style = { flexGrow = 1 } };
+					objField.objectType = GetObjectTypeForRefType(refType);
+					objField.allowSceneObjects = refType == EngineReferenceType.GameObject ||
+					                             refType == EngineReferenceType.Component;
+					objField.BindProperty(elem.FindPropertyRelative(nameof(InspectorReference.RefValue)));
+					objField.RegisterValueChangedCallback(evt => AutoFillRefName(index, evt.previousValue, evt.newValue));
+					valueElement = objField;
 					break;
 			}
 
+			valueElement.SetEnabled(!Application.isPlaying);
 			container.Add(valueElement);
 			EnsureCellInputSelectsRow(_referencesListView, container, index);
+		}
+
+		private void AutoFillRefName(Int32 index, Object prevValue, Object newValue)
+		{
+			if (Application.isPlaying)
+				return;
+
+			var so = serializedObject;
+			so.Update();
+			var nameProp = GetRefElemProp(index).FindPropertyRelative(nameof(InspectorReference.Name));
+
+			// Auto-fill name only when the name field is empty
+			var currentName = nameProp.stringValue;
+			if (String.IsNullOrEmpty(currentName) || prevValue != null && prevValue.name.Equals(currentName))
+			{
+				if (newValue != null)
+					nameProp.stringValue = newValue.name;
+			}
+
+			so.ApplyModifiedProperties();
 		}
 
 		private void BindRefTypeCell(VisualElement container, Int32 index)
 		{
 			container.Clear();
 
-			var row = _refViewRows[index];
+			var refTypeProp = GetRefElemProp(index).FindPropertyRelative(nameof(InspectorReference.RefType));
+			var refType = (EngineReferenceType)refTypeProp.enumValueIndex;
 
 			if (Application.isPlaying)
 			{
-				container.Add(new Label(row.RefType.ToString()) { style = { flexGrow = 1 } });
+				container.Add(new Label(refType.ToString()) { style = { flexGrow = 1 } });
 				return;
 			}
 
-			var enumField = new EnumField(row.RefType) { style = { flexGrow = 1 } };
+			var enumField = new EnumField(refType) { style = { flexGrow = 1 } };
 			enumField.RegisterValueChangedCallback(evt =>
 			{
 				var newType = (EngineReferenceType)evt.newValue;
-				Debug.LogWarning(newType);
 				var so = serializedObject;
 				so.Update();
-				var refsProp = so.FindProperty("_references");
-				var elem = refsProp.GetArrayElementAtIndex(index);
+				var elem = GetRefElemProp(index);
 				elem.FindPropertyRelative(nameof(InspectorReference.RefType)).enumValueIndex = (Int32)newType;
 				elem.FindPropertyRelative(nameof(InspectorReference.RefValue)).objectReferenceValue = null;
 				so.ApplyModifiedProperties();
@@ -198,60 +198,11 @@ namespace LunyScript.UnityEditor
 			EnsureCellInputSelectsRow(_referencesListView, container, index);
 		}
 
-		private void SetRefObjectValue(Int32 index, Object value)
-		{
-			var so = serializedObject;
-			so.Update();
-			var elem = so.FindProperty("_references").GetArrayElementAtIndex(index);
-			elem.FindPropertyRelative(nameof(InspectorReference.RefValue)).objectReferenceValue = value;
-
-			// Auto-fill name from object name if empty
-			var nameProp = elem.FindPropertyRelative(nameof(InspectorReference.Name));
-			if (String.IsNullOrEmpty(nameProp.stringValue) && value != null)
-				nameProp.stringValue = value.name;
-
-			so.ApplyModifiedProperties();
-			RefreshReferences();
-		}
-
-		private void SetRefColorValue(Int32 index, Color value)
-		{
-			var so = serializedObject;
-			so.Update();
-			so.FindProperty("_references")
-				.GetArrayElementAtIndex(index)
-				.FindPropertyRelative(nameof(InspectorReference.ColorValue))
-				.colorValue = value;
-			so.ApplyModifiedProperties();
-		}
-
-		private void SetRefCurveValue(Int32 index, AnimationCurve value)
-		{
-			var so = serializedObject;
-			so.Update();
-			so.FindProperty("_references")
-				.GetArrayElementAtIndex(index)
-				.FindPropertyRelative(nameof(InspectorReference.CurveValue))
-				.animationCurveValue = value;
-			so.ApplyModifiedProperties();
-		}
-
-		private void SetRefVector3Value(Int32 index, Vector3 value)
-		{
-			var so = serializedObject;
-			so.Update();
-			so.FindProperty("_references")
-				.GetArrayElementAtIndex(index)
-				.FindPropertyRelative(nameof(InspectorReference.Vector3Value))
-				.vector3Value = value;
-			so.ApplyModifiedProperties();
-		}
-
 		private void OnRefAddClicked()
 		{
 			var so = serializedObject;
 			so.Update();
-			var refsProp = so.FindProperty("_references");
+			var refsProp = GetRefsProp();
 			refsProp.InsertArrayElementAtIndex(refsProp.arraySize);
 			var newElem = refsProp.GetArrayElementAtIndex(refsProp.arraySize - 1);
 			newElem.FindPropertyRelative(nameof(InspectorReference.Name)).stringValue = String.Empty;
@@ -261,8 +212,8 @@ namespace LunyScript.UnityEditor
 			RefreshReferences();
 
 			_referencesListView.ClearSelection();
-			_referencesListView.SetSelection(_refViewRows.Count - 1);
-			_referencesListView.ScrollToItem(_refViewRows.Count - 1);
+			_referencesListView.SetSelection(_refCount - 1);
+			_referencesListView.ScrollToItem(_refCount - 1);
 		}
 
 		private void OnRefRemoveClicked()
@@ -273,7 +224,7 @@ namespace LunyScript.UnityEditor
 
 			var so = serializedObject;
 			so.Update();
-			var refsProp = so.FindProperty("_references");
+			var refsProp = GetRefsProp();
 			var newSelectionIndex = -1;
 
 			foreach (var index in _referencesListView.selectedIndices.OrderByDescending(i => i))
@@ -287,8 +238,8 @@ namespace LunyScript.UnityEditor
 			_referencesListView.ClearSelection();
 			RefreshReferences();
 
-			if (newSelectionIndex >= _refViewRows.Count)
-				newSelectionIndex = _refViewRows.Count - 1;
+			if (newSelectionIndex >= _refCount)
+				newSelectionIndex = _refCount - 1;
 			if (newSelectionIndex >= 0)
 			{
 				_referencesListView.SetSelection(newSelectionIndex);
